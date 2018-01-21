919abee6c14b8f82315820a089e8a561a47521e9
emuccino <33988506+emuccino@users.noreply.github.com>
2018-01-21 15:33:22 -0500

    Update post1_js.js

--- _includes/_js/post1_js.js
+++ _includes/_js/post1_js.js
@@ -1,1 +1,16 @@
-let java = "sample";
+$(document).ready(function() { $('div.x').click(function() {
+    $(this).removeClass("hidden");
+    let i = 0;
+    let j = 0;
+    while (i===0 && j<100) {
+    	let num = Math.floor((Math.random() * 9) + 1);
+    	console.log(num);
+    	if ($(`div.x.${num}`).hasClass("hidden") && $(`div.o.${num}`).hasClass("none")) {
+    		$(`div.o.${num}`).removeClass("none");
+    		$(`div.x.${num}`).addClass("none");
+    		i=1;
+    	}
+    	j+=1;
+    }
+});
+});
