5af8d68c585bb9b8b0f48527ffe9443780410035
emuccino <33988506+emuccino@users.noreply.github.com>
2018-02-02 21:32:42 -0500

    Update control.js

--- _includes/_js/control.js
+++ _includes/_js/control.js
@@ -1,16 +1,27 @@
 /*Global object contains all functions*/
 var globals = {};
 
+$(".reset").click(function() {
+    $(".cell").html('');
+    $(".cell").removeClass('occupied');
+    ui.intialControlsVisible = true;
+    ui.currentView = "";
+    $('.intial').fadeIn({
+        duration : "slow"
+    };
+    $(".level").addClass('not-selected');
+    $(".level").removeClass('selected');
+});
+
 /*Chose difficulty setting*/
 $(".level").each(function() {
-    var $this = $(this);
-    $this.click(function() {
+     $(this).click(function() {
         $('.selected').toggleClass('not-selected');
         $('.selected').toggleClass('selected');
-        $this.toggleClass('not-selected');
-        $this.toggleClass('selected');
+        $(this).toggleClass('not-selected');
+        $(this).toggleClass('selected');
 
-        ai.level = $this.attr("id");
+        ai.level = $(this).attr("id");
     });
 });
 
@@ -20,27 +31,21 @@ $(".start").click(function() {
     if(typeof selectedDiffeculty !== "undefined") {
         var aiPlayer = new AI(selectedDiffeculty);
         globals.game = new Game(aiPlayer);
-
         aiPlayer.plays(globals.game);
-
         globals.game.start();
     }
 });
 
-/*CLick on cell to place letter, advance game*/
+/*CLick on cell to place letter, advance game */
  $(".cell").each(function() {
      var $this = $(this);
      $this.click(function() {
          if(globals.game.status === "running" && globals.game.currentState.turn === "X" && !$this.hasClass('occupied')) {
              var indx = parseInt($this.data("indx"));
-
              var next = new State(globals.game.currentState);
              next.board[indx] = "X";
-
              ui.insertAt(indx, "X");
-
              next.advanceTurn();
-
              globals.game.advanceTo(next);
 
          }
