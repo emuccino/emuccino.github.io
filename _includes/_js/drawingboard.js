da49288e9a3cd63324050aea59dbe61d46e8e7aa
emuccino <33988506+emuccino@users.noreply.github.com>
2018-02-04 15:14:56 -0500

    Update drawingboard.js

--- _includes/_js/drawingboard.js
+++ _includes/_js/drawingboard.js
@@ -1,8 +1,42 @@
+var opac;
 $(".cell").mousedown(function() {
-  $(this).css("background-color", "black");
-  $(".cell").mouseover(function() {
-    $(this).css("background-color", "black");
-  });
+
+    var dat = parseFloat($(this).data("indx"));
+    var vals = [dat-28,dat+28,dat-1,dat+1];
+    var vals1 = [dat-29,dat+29,dat-27,dat+27];
+
+    $(this).css("opacity", parseFloat($(this).css("opacity")) + 1);
+
+    if (27 > dat < 756) {
+        vals.forEach(function(item) {
+            $('[data-indx =' + String(item) + ']').css("opacity", parseFloat($('[data-indx =' + String(item) + ']').css("opacity")) + 0.5);
+        });
+
+        vals1.forEach(function(item) {
+           $('[data-indx =' + String(item) + ']').css("opacity", parseFloat($('[data-indx =' + String(item) + ']').css("opacity")) + 0.25);
+        });
+    }
+
+    $(".cell").mouseover(function() {
+        var dat = parseFloat($(this).data("indx"));
+        var vals = [dat-28,dat+28,dat-1,dat+1];
+        var vals1 = [dat-29,dat+29,dat-27,dat+27];
+
+        opac = $(this).css("opacity");
+        $(this).css("opacity", parseFloat(opac) + 1);
+
+        if (27 > dat < 756) {
+            vals.forEach(function(item) {
+                console.log($('[data-indx =' + String(item) + ']').data("indx"));
+                $('[data-indx =' + String(item) + ']').css("opacity", parseFloat($('[data-indx =' + String(item) + ']').css("opacity")) + 0.5);
+            });
+
+            vals1.forEach(function(item) {
+               $('[data-indx =' + String(item) + ']').css("opacity", parseFloat($('[data-indx =' + String(item) + ']').css("opacity")) + 0.25);
+            });
+        }
+
+    });
 }).mouseup(function() {
-  $(".cell").unbind('mouseover');
+    $(".cell").unbind('mouseover');
 });
