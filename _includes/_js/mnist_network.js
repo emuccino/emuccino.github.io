7023dcaf42f19d75b851b123a081ac63ffc5294d
emuccino <33988506+emuccino@users.noreply.github.com>
2018-03-13 00:59:45 -0400

    Update mnist_network.js

--- _includes/_js/mnist_network.js
+++ _includes/_js/mnist_network.js
@@ -10,15 +10,15 @@ function appendones(m) {
 	return result;
 }
 
-function logistic(m) {
+function relu(m) {
 	var result = [];
 
 	for(y = 0; y < m.length; y++) {
 		var result_row = []
 
 		for(x = 0; x < m[y].length; x++) {
-			var f = 1.0 / (1.0 + Math.exp(-m[y][x]));
-			result_row.push(f);
+			f = Math.max(0, m[y][x])
+			result_row.push(f)
 		}
 		result.push(result_row);
 	}
